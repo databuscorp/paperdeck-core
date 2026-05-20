6f6e434fe6bb5b68f9741fc681b61097f2dd5fae
kmani-code <112385209+kmani-code@users.noreply.github.com>
2026-05-20 13:45:46 +0530

    Update settings.py

--- paperdeck/settings.py
+++ paperdeck/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-paperdeck-dev-key-change-in-production')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost','api.paperdeck.databus.co','paperdeck.databus.co').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
